bc642cf81aaf975347252230837f2a8344329b6b
Krzysztof Tybinka <ktybinka@interia.pl>
2023-02-08 10:33:49 +0100

    IFileParser interface added

---
+++ FileAnalyzer/IFileParser.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileAnalyzer
+{
+    public interface IFileParser
+    {
+        List<string> ParseFile(string filePath, string attribute);
+    }
+}
