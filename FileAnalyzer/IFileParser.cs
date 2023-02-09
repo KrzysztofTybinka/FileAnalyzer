4266ec85a843b47742ca779d015cf820d806a847
Krzysztof Tybinka <ktybinka@interia.pl>
2023-02-09 01:38:16 +0100

    IFileReader interface summarized

--- FileAnalyzer/IFileParser.cs
+++ FileAnalyzer/IFileParser.cs
@@ -6,9 +6,23 @@ using System.Threading.Tasks;
 
 namespace FileAnalyzer
 {
+    /// <summary>
+    /// Provides methods for file parsing.
+    /// </summary>
     public interface IFileParser
     {
+        /// <summary>
+        /// Gets file from directory.
+        /// </summary>
+        /// <param name="path"></param>
+        /// <returns>String representation of file content.</returns>
         public string GetFile(string path);
+
+        /// <summary>
+        /// Parses given attribute values from file into string list.
+        /// </summary>
+        /// <param name="attribute"></param>
+        /// <returns>List with parsed attribute values.</returns>
         public List<string> ParseFile(string attribute);
     }
 }
--- FileAnalyzer/Phrase.cs
+++ FileAnalyzer/Phrase.cs
@@ -8,8 +8,18 @@ using System.Threading.Tasks;
 
 namespace FileAnalyzer
 {
+    /// <summary>
+    /// Provides methods for input data interpretation.
+    /// </summary>
     public class Phrase
     {
+        /// <summary>
+        /// Interprets input value and returns list of 
+        /// values that are a result of input interpretation.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException"></exception>
         public List<string> Interpret(string input)
         {
             try
@@ -31,6 +41,15 @@ namespace FileAnalyzer
             }
         }
 
+        /// <summary>
+        /// Calls proper method based on given function name.
+        /// </summary>
+        /// <param name="attribute"></param>
+        /// <param name="method"></param>
+        /// <param name="value"></param>
+        /// <param name="service"></param>
+        /// <returns>List containing string representation of objects obtained by input function.</returns>
+        /// <exception cref="InvalidOperationException"></exception>
         private List<string> Call(string attribute, string method, string value, FileService service)
         {
             switch (method)
@@ -49,6 +68,13 @@ namespace FileAnalyzer
             }
         }
 
+        /// <summary>
+        /// Creates proper FileParser object.
+        /// </summary>
+        /// <param name="fileType"></param>
+        /// <param name="path"></param>
+        /// <returns>IFileParser.</returns>
+        /// <exception cref="InvalidOperationException"></exception>
         private IFileParser GetParser(string fileType, string path)
         {
             switch (fileType)
