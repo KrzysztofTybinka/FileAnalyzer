64092d554b07e8e3b77edc0e20df2489fc221ba2
Krzysztof Tybinka <ktybinka@interia.pl>
2023-02-12 19:42:54 +0100

    GetFile method removed

--- FileAnalyzer/IFileParser.cs
+++ FileAnalyzer/IFileParser.cs
@@ -12,13 +12,6 @@ namespace FileAnalyzer
     public interface IFileParser
     {
         /// <summary>
-        /// Gets file from directory.
-        /// </summary>
-        /// <param name="path"></param>
-        /// <returns>String representation of file content.</returns>
-        public string GetFile(string path);
-
-        /// <summary>
         /// Parses given attribute values from file into string list.
         /// </summary>
         /// <param name="attribute"></param>
