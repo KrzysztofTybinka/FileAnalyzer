ac244a7858069eca993ec4f1e21fdd1a242c3cfa
Krzysztof Tybinka <ktybinka@interia.pl>
2023-02-12 21:36:52 +0100

    Test method added

--- FileAnalyzerTests/JsonFileParser_Tests.cs
+++ FileAnalyzerTests/JsonFileParser_Tests.cs
@@ -1,11 +1,28 @@
+using FileAnalyzer;
+
 namespace FileAnalyzerTests
 {
     public class JsonFileParser_Tests
     {
-        [Fact]
-        public void Test1()
+        public class JsonFileParserTests
         {
+            [Fact]
+            public void ParseFile_ReturnsListOfCities_WhenGivenCityKey()
+            {
+                // Arrange
+                string jsonString = Resources.GetJsonString();
+                JsonFileParser parser = new JsonFileParser(jsonString);
 
+                // Act
+                List<string?> cities = parser.ParseFile("city");
+
+                // Assert
+                Assert.Collection(cities,
+                    city => Assert.Equal("Anytown", city),
+                    city => Assert.Equal("Someplace", city),
+                    city => Assert.Equal("Anotherplace", city),
+                    city => Assert.Equal("Nowhere", city));
+            }
         }
     }
 }
