e9c29d83878766314c485ca0d7dd38edb5627045
Krzysztof Tybinka <ktybinka@interia.pl>
2023-02-12 19:40:51 +0100

    Class summarized

--- FileAnalyzer/FileDownloader.cs
+++ FileAnalyzer/FileDownloader.cs
@@ -7,11 +7,18 @@ using System.Threading.Tasks;
 namespace FileAnalyzer
 {
     /// <summary>
-    /// Provides methods for downloading files.
+    /// Provides methods for 
+    /// downloading file content.
     /// </summary>
     public class FileDownloader
     {
-        public string Download(string input)
+        /// <summary>
+        /// Gets string representation of file content.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>String representation of file content.</returns>
+        /// <exception cref="InvalidOperationException"></exception>
+        public string GetContent(string input)
         {
             if (IsUrl(input))
             {
@@ -27,6 +34,12 @@ namespace FileAnalyzer
             }
         }
 
+        /// <summary>
+        /// Checks if input value is url address.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>True if input walue is url, otherwise false.</returns>
+        /// <exception cref="NullReferenceException"></exception>
         private bool IsUrl(string input)
         {
             return Uri.TryCreate(input, UriKind.Absolute, out Uri? uriResult)
@@ -34,11 +47,22 @@ namespace FileAnalyzer
                 || uriResult.Scheme == (Uri.UriSchemeHttps ?? throw new NullReferenceException()));
         }
 
+        /// <summary>
+        /// Checks if input value is 
+        /// valid directory path.
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>True if input is valid, otherwise false.</returns>
         private bool IsValidPath(string input)
         {
             return File.Exists(input);
         }
 
+        /// <summary>
+        /// Downloads file as a string from given url.
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns>File content as a string.</returns>
         private async Task<string> DownloadFromUrl(string url)
         {
             using (HttpClient client = new HttpClient())
@@ -47,6 +71,11 @@ namespace FileAnalyzer
             }
         }
 
+        /// <summary>
+        /// Downloads file as a string from specified directory.
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns>File content as a string.</returns>
         private string DownloadFromDir(string filePath)
         {
             return File.ReadAllText(filePath);
