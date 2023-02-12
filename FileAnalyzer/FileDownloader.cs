7dfbd1a076cf14b98294cfe8817379391b3a434e
Krzysztof Tybinka <ktybinka@interia.pl>
2023-02-12 19:14:15 +0100

    Download method added

--- FileAnalyzer/FileDownloader.cs
+++ FileAnalyzer/FileDownloader.cs
@@ -6,7 +6,50 @@ using System.Threading.Tasks;
 
 namespace FileAnalyzer
 {
-    internal class FileDownloader
+    /// <summary>
+    /// Provides methods for downloading files.
+    /// </summary>
+    public class FileDownloader
     {
+        public string Download(string input)
+        {
+            if (IsUrl(input))
+            {
+                return DownloadFromUrl(input).Result;
+            }
+            else if (IsValidPath(input))
+            {
+                return DownloadFromDir(input);
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        private bool IsUrl(string input)
+        {
+            return Uri.TryCreate(input, UriKind.Absolute, out Uri? uriResult)
+                && (uriResult.Scheme == (Uri.UriSchemeHttp ?? throw new NullReferenceException())
+                || uriResult.Scheme == (Uri.UriSchemeHttps ?? throw new NullReferenceException()));
+        }
+
+        private bool IsValidPath(string input)
+        {
+            return File.Exists(input);
+        }
+
+        private async Task<string> DownloadFromUrl(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                return await client.GetStringAsync(url);
+            }
+        }
+
+        private string DownloadFromDir(string filePath)
+        {
+            return File.ReadAllText(filePath);
+        }
     }
 }
